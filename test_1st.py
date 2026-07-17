ee8aa0dc045d3057df021f015efb4459117ac6c4
priya595 <bala.priya90@gmail.com>
2026-07-17 18:27:08 -0400

    test_1st.py

---
+++ test_1st.py
@@ -0,0 +1,11 @@
+print("inside test_1st.py")
+
+h = input("Enter yes or no if you are hungry")
+if h == "yes":
+    print("eat samosa")
+    print("eat pizza")
+    print("eat bread")
+else:
+    thirsty = input("Enter if are you thirsty :")
+    if thirsty == 'yes':
+        print("Drink water")
